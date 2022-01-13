a5d45060846aceb12556d10473396912d24530ae
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-01-13 17:01:08 +0530

    Add files via upload

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nav = ({ setLibraryStatus, libraryStatus }) => {
+  const openLibraryHandler = () => {
+    setLibraryStatus(!libraryStatus);
+  };
+
+  return (
+    <nav>
+      <h1>Reactive</h1>
+      <button
+        className={libraryStatus ? "library-active" : ""}
+        onClick={openLibraryHandler}
+      >
+        Library
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
